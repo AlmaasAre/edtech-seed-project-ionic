2ee11e5632cbd912b2046136a439a268f42bb574
Are Almaas <mralmaas@gmail.com>
2015-04-09 15:07:39 +0200

    add typedefinitions

--- gulp-config.js
+++ gulp-config.js
@@ -1,5 +1,12 @@
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  typescript:{
+    definitions:[
+      'www/lib/dt-jquery/dt-jquery.d.ts',
+      'www/lib/dt-angular/angular.d.ts',
+      'www/lib/dt-angular/angular-animate.d.ts'
+    ]
+  }
 };
 
 module.exports = paths;
